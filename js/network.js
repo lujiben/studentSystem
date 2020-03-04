755792849ad377ada168744f04c28f3e4bdc5526
CzongZ <1215263244@qq.com>
2020-03-04 14:23:21 +0800

    豆瓣电影

---
+++ js/network.js
@@ -0,0 +1,13 @@
+let devUrl1 = 'https://douban.uieee.com';
+let devUrl2 
+let testUrl
+let proUrl1
+let proUrl2
+let baseUrl = devUrl1;
+
+let us_box = `${baseUrl}/v2/movie/us_box`;
+let weekly = `${baseUrl}/v2/movie/weekly`;
+let top250 = `${baseUrl}/v2/movie/top250`;
+let new_movies = `${baseUrl}/v2/movie/new_movies`;
+let in_theaters = `${baseUrl}/v2/movie/in_theaters`;
+let coming_soon = `${baseUrl}/v2/movie/coming_soon`;
